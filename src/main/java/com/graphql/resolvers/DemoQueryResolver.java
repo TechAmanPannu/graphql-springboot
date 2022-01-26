bbad5338207b6cd3481a3cdaa9bfc67bc4fc9f23
Aman <AmanTechPannu>
2022-01-26 19:54:01 +0530

    created demo query resolver

--- src/main/java/com/graphql/resolvers/DemoQueryResolver.java
+++ src/main/java/com/graphql/resolvers/DemoQueryResolver.java
@@ -1,11 +1,17 @@
 package com.graphql.resolvers;
 
+import com.graphql.dao.DemoDao;
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DemoQueryResolver implements GraphQLQueryResolver {
 
+    private DemoDao demoDao;
 
-    String getUserById(String id) { return id; }
+    public DemoQueryResolver(DemoDao demoDao) {
+        this.demoDao = demoDao;
+    }
+
+    String doSomething() { return demoDao.doSomething(); }
 }
