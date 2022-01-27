eebe06def1e1466f23f404b7b85c0d96ba672e6c
Aman <AmanTechPannu>
2022-01-27 10:51:37 +0530

    rest controller setup

---
+++ src/main/java/com/graphql/controllers/DemoEndpoint.java
@@ -0,0 +1,31 @@
+package com.graphql.controllers;
+
+
+import com.graphql.dao.DemoDao;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoEndpoint {
+
+    private DemoDao demoDao;
+
+    public DemoEndpoint(DemoDao demoDao) {
+        this.demoDao = demoDao;
+    }
+
+    @GetMapping(value = "/do-something")
+    public ResponseEntity doSomething() {
+        return ResponseEntity.ok(demoDao.doSomething());
+    }
+
+    @GetMapping(value = "/do-nothing")
+    public ResponseEntity doNothing() {
+        return ResponseEntity.ok(demoDao.doNothing());
+    }
+}
